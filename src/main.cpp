e436f8cfd6e820e295aa7613cf5a1a2e1070d66f
H1rono <hronok66@gmail.com>
2022-06-28 17:43:07 +0900

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(6, OUTPUT);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    digitalWrite(6, HIGH);
+    delay(500);
+    digitalWrite(6, LOW);
+    delay(500);
+}
